722962ac3ea44139a4d3875bb6a59a0e5a2751f1
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-15 17:29:03 +0200

    clean

--- destination.php
+++ destination.php
@@ -1,12 +1,13 @@
 <?php 
-$text = $_POST['text'];
-$text_length = strlen($text);
-$censored = $_POST['censored'];
-$text_censored = str_replace($censored,'***', $text);
+$text =                  $_POST['text'];
+$text_length =          strlen($text);
+$censored =              $_POST['censored'];
+$text_censored =        str_replace($censored,'***', $text);
 $text_censored_length = strlen($text_censored);
-$text_shuffled = str_shuffle($text);
-$text_words = str_word_count($text, 1, 'àáãç3')
+$text_shuffled =        str_shuffle($text);
+$text_words =           str_word_count($text, 1, 'àáãç3')
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,17 +18,30 @@ $text_words = str_word_count($text, 1, 'àáãç3')
 </head>
 <body>
   <div class="container" style="font-family: monospace;">
+
     <h3>testo in entrata</h3>
-    <p><?php echo ucfirst($text) ?> <br> lungo <?php echo $text_length ?> lettere</p>
+    <p>
+      <?php echo ucfirst($text)?> <br> lungo 
+      <?php echo $text_length ?> lettere
+    </p>
 
     <h3>testo censurato</h3>
-    <p><?php echo ucfirst($text_censored)  ?> <br> lungo <?php echo $text_censored_length ?> lettere</p>
+    <p>
+      <?php echo ucfirst($text_censored)?> <br> lungo 
+      <?php echo $text_censored_length ?> lettere
+    </p>
 
     <h3>testo shuffled</h3>
-    <p><?php echo ucfirst($text_shuffled)  ?> <br> lungo <?php echo strlen($text_shuffled)?> lettere</p>
+    <p>
+      <?php echo ucfirst($text_shuffled)?> <br> lungo 
+      <?php echo strlen($text_shuffled)?> lettere
+    </p>
 
     <h3>parole in array</h3>
-    <?php echo var_dump($text_words) ?>
+    <p>
+      <?php echo var_dump($text_words)?>
+    </p>
+
   </div>
 </body>
 </html>
